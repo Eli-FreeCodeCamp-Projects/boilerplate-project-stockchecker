ff36c086c15760b013d157481c637170fd795289
Eli <eli.serra173@gmail.com>
2023-11-02 18:17:31 +0100

    remove deprecated mongoose connection parameters

--- server.js
+++ server.js
@@ -32,8 +32,7 @@ app.use(helmet({
 }));
 
 mongoose.connect(
-  process.env['MONGO_URI'],
-  { useNewUrlParser: true, useUnifiedTopology: true })
+  process.env['MONGO_URI'])
     .then(()=>{
       console.log("We are connected to mongo db")
     })
